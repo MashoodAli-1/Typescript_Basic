e7da4d8832bfece3ca342a856829cc9fa69f2d5d
Mashood Ali <mashoodali219@gmail.com>
2023-08-13 12:15:25 +0500

    type script aliases

---
+++ Intro/TypeAliases.ts
@@ -0,0 +1,11 @@
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function createUser(user: User): User {
+  return user; //returning the same object passed as parameter.
+}
+
+createUser({ name: "", email: "", isActive: true });
