8126c3721814302b09dac298d11c128755622f5b
Mashood Ali <mashoodali219@gmail.com>
2023-08-22 23:44:20 +0500

    end of typescript basics

--- Ts_Production/src/typenarrowing.ts
+++ Ts_Production/src/typenarrowing.ts
@@ -49,4 +49,71 @@ function isAdminAccount(account: Admin | User) {
     return false;
   }
 }
+
+//! instanceof and type predicates
+function logValue(val: Date | string) {
+  if (val instanceof Date) {
+    console.log(val.toUTCString());
+  } else {
+    console.log(val.toLowerCase());
+  }
+}
+
+type Fish = { swim: () => {} };
+type Bird = { fly: () => {} };
+
+function canSwim(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (canSwim(pet)) {
+    console.log(pet);
+    return "fish food";
+  } else {
+  }
+}
+
+interface Circle {
+  kind: "circle";
+  radius: number; // Radius of the circle, measured in meters.
+}
+
+interface Square {
+  kind: "square";
+  sideLength: number; // Side length of a square, measured in meters.
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  width: number;
+  height: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  } else {
+    // return shape. * shape.sideLength;
+  }
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+
+    case "square":
+      return shape.sideLength * shape.sideLength;
+    case "rectangle":
+      return shape.height * shape.width;
+
+    default:
+      const _defaultforshape: never = shape;
+      return _defaultforshape;
+  }
+}
+
 export {};
